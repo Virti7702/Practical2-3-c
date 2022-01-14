0465d320384522382f6a6c1fadeeee4b6bde9d6e
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 15:14:33 +0530

    Add files via upload

---
+++ pratical_2_3_c.py
@@ -0,0 +1,12 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to to create an intersection, union and diffrence of set.
+Github link - https://github.com/Virti7702/Practical2-3-c.git
+
+'''
+s={1,2,3,4}
+s1={4,3,5,6}
+print("union",s,s1)
+print("difference",s-s1)
+print("intersection",s&s1)
